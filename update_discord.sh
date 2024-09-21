c33b997875a92c988da75664a7fea8e299a01ae7
CowCat <114646151+CowCat271@users.noreply.github.com>
2024-09-21 07:17:40 +0300

    script for updating discord
    This bash script downloads the last version of discord and installs it on Debian system using Nala front-end for 'libapt-pkg.'

---
+++ update_discord.sh
@@ -0,0 +1,13 @@
+discord_ver_dir="/home/cowcat271/Downloads/Discord_versions"
+discord_ver_file=new_discord.deb
+
+# mkdir --parents --verbose $discord_ver_dir
+# rm --force --verbose $discord_ver_dir/$discord_ver_file
+wget --output-document=$discord_ver_file --verbose "https://discord.com/api/download?platform=linux&format=deb"
+echo "New discord version downloaded"
+
+sudo nala install --assume-yes --verbose ./$discord_ver_file
+echo "New discord version has been installed successfully."
+
+rm --force --verbose ./$discord_ver_file
+echo "Downloaded file deleted"
