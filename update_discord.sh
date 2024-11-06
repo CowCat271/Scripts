3844143540f082df03ca6e2225a75b38ad7766d2
CowCat <114646151+CowCat271@users.noreply.github.com>
2024-11-06 23:48:55 +0200

    adding commits for better readability

--- update_discord.sh
+++ update_discord.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-discord_ver_dir="/home/cowcat271/Downloads/Discord_versions"
+#discord_ver_dir="/home/cowcat271/Downloads/Discord_versions"
 discord_ver_file=new_discord.deb
 
 # mkdir --parents --verbose $discord_ver_dir
 # rm --force --verbose $discord_ver_dir/$discord_ver_file
+
+# Download the latest version of Discord for Linux (Debian package format)
 wget --output-document=$discord_ver_file --verbose "https://discord.com/api/download?platform=linux&format=deb"
 echo "New discord version downloaded"
 
@@ -13,3 +15,15 @@ echo "New discord version has been installed successfully."
 
 rm --force --verbose ./$discord_ver_file
 echo "Downloaded file deleted"
+
+# Prompt the user to open Discord, defaulting to "yes" if no input is provided
+read -p "Open Discord now? (y/n): " open_discord
+open_discord=${open_discord:-y}
+
+# Check if the user wants to open Discord; open it in the background if they do
+if [[ "$open_discord" =~ ^[Yy]$ ]]; then
+    nohup discord &> /dev/null &
+    echo "Discord has been opened."
+else
+    echo "Discord installation complete. Not opening Discord."
+fi
