3e97856b1a5209b0b5ace326cdc6803b4da6c104
CowCat <114646151+CowCat271@users.noreply.github.com>
2024-11-26 16:10:05 +0200

    Update update_discord.sh

--- update_discord.sh
+++ update_discord.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-#discord_ver_dir="/home/cowcat271/Downloads/Discord_versions"
 discord_ver_file=new_discord.deb
-
-# mkdir --parents --verbose $discord_ver_dir
-# rm --force --verbose $discord_ver_dir/$discord_ver_file
 
 # Download the latest version of Discord for Linux (Debian package format)
 wget --output-document=$discord_ver_file --verbose "https://discord.com/api/download?platform=linux&format=deb"
@@ -16,8 +12,8 @@ echo "New discord version has been installed successfully."
 rm --force --verbose ./$discord_ver_file
 echo "Downloaded file deleted"
 
-# Prompt the user to open Discord, defaulting to "yes" if no input is provided
-read -p "Open Discord now? (y/n): " open_discord
+# Prompt the user to open Discord, defaulting to "yes" if no input is provided within 10 seconds
+read -t 10 -p "Open Discord now? (Y/n): " open_discord 
 open_discord=${open_discord:-y}
 
 # Check if the user wants to open Discord; open it in the background if they do
